473cad6aa2b66904bc953f8f28d8c3aba06b93ae
shreyman21 <shuklashreyas@icloud.com>
2024-09-25 22:11:03 -0400

    first steps

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+import alpaca_trade_api as tradeapi
+from alpaca_trade_api.rest import TimeFrame
+
+
+load_dotenv()
+
+# Access the variables
+api_key = os.getenv('APCA_API_KEY_ID')
+api_secret = os.getenv('APCA_API_SECRET_KEY')
+base_url = os.getenv('APCA_API_BASE_URL')
+
+
+api = tradeapi.REST(api_key, api_secret, base_url, api_version='v2')
+
+# Fetch the latest bars for AAPL stock
+aapl_bars = api.get_bars("AAPL", TimeFrame.Minute, limit=5).df
+
+
+for index, bar in aapl_bars.iterrows():
+    print(f"Time: {index}, Close: {bar['close']}")
