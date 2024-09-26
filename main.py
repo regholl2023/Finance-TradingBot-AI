1a97198dde8802891c45917ada8fc4cd803953b5
shreyman21 <shuklashreyas@icloud.com>
2024-09-26 11:49:14 -0400

    new changes

--- main.py
+++ main.py
@@ -14,9 +14,19 @@ base_url = os.getenv('APCA_API_BASE_URL')
 
 api = tradeapi.REST(api_key, api_secret, base_url, api_version='v2')
 
-# Fetch the latest bars for AAPL stock
-aapl_bars = api.get_bars("AAPL", TimeFrame.Minute, limit=5).df
+# Fetch the latest bars for BLNK stock
+aapl_bars = api.get_bars("BLNK", TimeFrame.Minute, limit=5).df
 
 
 for index, bar in aapl_bars.iterrows():
     print(f"Time: {index}, Close: {bar['close']}")
+
+account = api.get_account()
+print(account.buying_power)
+
+clock = api.get_clock()
+print('The market is {}'.format('open.' if clock.is_open else 'closed.'))
+
+
+orders = api.list_orders(status='open')
+print(orders)
